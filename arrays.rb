83b511360f5f752833faf41b9ef12b21e4f56846
@networkchaos <65135432+networkchaos@users.noreply.github.com>
2023-09-11 10:34:23 +0300

    fisrt commit

---
+++ arrays.rb
@@ -0,0 +1,12 @@
+#creating arrays
+
+num_array = [1,2,3,4,5]
+
+str_array = ["this ", "is ", "a", " small""array"]
+
+
+#array can also be created by:
+Array.new
+
+#you can add parameters to the new method (3,7
+# the 3 is to name the numbers of array size and the 7 is assign the nil values in the array named by 3)
---
+++ loops.rb
@@ -0,0 +1,39 @@
+#loop loop in ruby
+
+
+i= 30
+loop do
+  puts "i is #{1}"
+  i += 10
+  break if 1 == 80
+
+end
+
+# while loop in ruby
+
+i = 0
+while i < 10 do
+  puts "i is #{i}"
+  i += 1
+end
+
+#until loop in ruby
+#this loop continues as long the condition is false unlike the while loop
+i = 0
+until >= 10 do
+  puts " i is #{i}"
+  i += 1
+end
+
+i = 40
+loop do
+  puts "#{i}"
+  i++
+ break if
+end
+
+
+5.times do
+  puts "#{number}"
+
+end
